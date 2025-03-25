36b3982f9b65dc261b022ca78d56537bf0eaeb0b
Andries Bester <dries@thephantompress.com>
2025-03-25 14:34:12 +0200

    v1 rework

--- archive-transactions.php
+++ archive-transactions.php
@@ -50,7 +50,6 @@ if (have_posts()) :
     $rand_invested_data_json = json_encode(array_reverse($rand_invested_data), JSON_NUMERIC_CHECK);
     $value_on_luno_data_json = json_encode(array_reverse($value_on_luno_data), JSON_NUMERIC_CHECK);
 ?>
-
 <!-- Google Fonts Ubuntu -->
 <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
 <style>
@@ -61,27 +60,22 @@ if (have_posts()) :
 
 <div class="container py-5">
 
-    <!-- Main Heading -->
     <div class="mb-5 text-center">
         <h1 class="fw-bold">Vakansie Yes!</h1>
     </div>
 
-    <!-- First Row: BTC Panel and Chart with equal height and centered stats -->
     <div class="row g-4 align-items-stretch">
-        <!-- Left Column: BTC Info centered -->
         <div class="col-lg-6">
             <div class="p-4 bg-light rounded shadow h-100 d-flex align-items-center justify-content-center text-center">
                 <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png" alt="Bitcoin Logo" class="img-fluid mb-3" style="max-width: 250px; height: auto;">
-
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png" alt="Bitcoin Logo" class="img-fluid mb-3" style="max-width: 150px; height: auto;">
                     <?php if ($btc_price): ?>
                         <h4 style="color: #EF8F18;">Current BTC Price: R<?php echo number_format($btc_price, 2, '.', ','); ?></h4>
                     <?php else: ?>
                         <h4 class="text-danger">BTC price unavailable</h4>
                     <?php endif; ?>
 
-                    <?php
-                    if ($first_btc_owned_entry !== null && $btc_price):
+                    <?php if ($first_btc_owned_entry !== null && $btc_price):
                         $current_holdings_value = $first_btc_owned_entry * $btc_price;
                         $value_per_person = $current_holdings_value / 10;
                         $btc_per_person = $first_btc_owned_entry / 10;
@@ -94,7 +88,6 @@ if (have_posts()) :
             </div>
         </div>
 
-        <!-- Right Column: Chart -->
         <div class="col-lg-6">
             <div class="p-4 bg-light rounded shadow h-100">
                 <canvas id="transactionChart" width="400" height="400"></canvas>
@@ -102,10 +95,9 @@ if (have_posts()) :
         </div>
     </div>
 
-    <!-- Second Row: Table and Mock Text -->
+    <!-- Full Width Table -->
     <div class="row g-4 mt-4">
-        <!-- Left Column: Table -->
-        <div class="col-lg-8">
+        <div class="col-12">
             <div class="table-responsive bg-white rounded shadow p-3">
                 <table class="table table-striped table-hover align-middle">
                     <thead style="background-color: #EF8F18; color: #000;">
@@ -118,8 +110,7 @@ if (have_posts()) :
                         </tr>
                     </thead>
                     <tbody>
-                        <?php rewind_posts(); ?>
-                        <?php while (have_posts()) : the_post();
+                        <?php rewind_posts(); while (have_posts()) : the_post();
                             $purchase_date = get_field('purchase_date');
                             $rand_amount_invested = get_field('rand_amount_invested');
                             $amount_of_btc_owned = get_field('amount_of_btc_owned');
@@ -137,24 +128,15 @@ if (have_posts()) :
                                 <td>R<?php echo esc_html(number_format($rand_amount_invested, 2)); ?></td>
                                 <td><?php echo esc_html(number_format($amount_of_btc_owned, 8)); ?> BTC</td>
                                 <td>R<?php echo esc_html(number_format($value_on_luno, 2)); ?></td>
-                                <td class="<?php echo $percentage_class; ?>">
-                                    <?php echo esc_html(number_format($percentage_increase, 2)); ?>%
-                                </td>
+                                <td class="<?php echo $percentage_class; ?>"><?php echo esc_html(number_format($percentage_increase, 2)); ?>%</td>
                             </tr>
                         <?php endwhile; ?>
                     </tbody>
                 </table>
             </div>
         </div>
-
-        <!-- Right Column: Mock Text -->
-        <div class="col-lg-4">
-            <div class="p-4 bg-light rounded shadow h-100">
-                <h5>Placeholder Content</h5>
-                <p>This is a placeholder block. You can insert stats, notes, or additional charts here later.</p>
-            </div>
-        </div>
     </div>
+
 </div>
 
 <script>
@@ -168,13 +150,13 @@ if (have_posts()) :
                 datasets: [{
                     label: 'Rand Amount Invested',
                     data: <?php echo $rand_invested_data_json; ?>,
-                    borderColor: '#000000', // Black
+                    borderColor: '#000000',
                     fill: false,
                     tension: 0.3
                 }, {
                     label: 'Value on Luno',
                     data: <?php echo $value_on_luno_data_json; ?>,
-                    borderColor: '#EF8F18', // Bitcoin Orange
+                    borderColor: '#EF8F18',
                     fill: false,
                     tension: 0.3
                 }]
